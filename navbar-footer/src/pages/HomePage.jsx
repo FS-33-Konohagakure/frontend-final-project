e9176d89e12b04027466a2e156ee5a3008bd5e84
noormalfaz <noorm4210@gmail.com>
2023-11-21 12:39:55 +0700

    Create logo informasi kesehatan

--- navbar-footer/src/pages/HomePage.jsx
+++ navbar-footer/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import HeroSection from "../components/hero-section/HeroSection";
+import InformasiKesehatan from "../components/informasi-kesehatan/InformasiKesehatan";
 
 function HomePage() {
   return (
     <Layout>
       <HeroSection />
+      <InformasiKesehatan />
     </Layout>
   );
 }
--- navbar-footer/tailwind.config.js
+++ navbar-footer/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         "blue-theme": "#0B8FAC",
         "blue-theme-hover": "#0A6D83",
         "blue-theme-transparant": "#0b8faca2",
+        "green-theme": "#5AAE73",
       },
       boxShadow: {
         md: "0 0 10px rgba(0, 0, 0, 0.1)",
